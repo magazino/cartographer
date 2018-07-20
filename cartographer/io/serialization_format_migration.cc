321f9b16c6c1c33bd910a2651e0e2ca185053f48
Michael Grupp <grupp@magazino.eu>
2018-07-20 18:01:41 +0200

    clean up and command line flag

--- cartographer/io/serialization_format_migration.cc
+++ cartographer/io/serialization_format_migration.cc
@@ -22,8 +22,10 @@
 
 #include "cartographer/common/math.h"
 #include "cartographer/mapping/2d/submap_2d.h"
+#include "cartographer/mapping/probability_values.h"
 #include "cartographer/mapping/proto/internal/legacy_probability_grid.pb.h"
 #include "cartographer/mapping/proto/internal/legacy_serialized_data.pb.h"
+#include "cartographer/mapping/proto/internal/legacy_serialized_data_old_submap.pb.h"
 #include "cartographer/mapping/proto/internal/legacy_submap.pb.h"
 #include "cartographer/mapping/proto/trajectory_builder_options.pb.h"
 #include "glog/logging.h"
@@ -54,69 +56,56 @@ bool ReadBuilderOptions(
 
 // Checks if the proto is in the old "probability-grid-only" 2D submap format
 // that was used before the generalized grid field was introduced.
-bool HasLegacyProbabilityGrid2d(const mapping::proto::Submap& submap) {
+bool HasLegacyProbabilityGrid2d(const mapping::proto::LegacySubmap& submap) {
   if (submap.has_submap_2d()) {
-    if (!submap.submap_2d().grid().has_probability_grid_2d() &&
-        !submap.submap_2d().grid().has_tsdf_2d()) {
+    if (submap.submap_2d().has_probability_grid()) {
       return true;
     }
   }
   return false;
 }
 
-mapping::proto::Submap MaybeMigrateLegacySubmap2d(
+mapping::proto::Submap MigrateLegacySubmap2d(
     const mapping::proto::LegacySubmap& submap_in) {
-  if (true) {
-  //if (HasLegacyProbabilityGrid2d(submap_in)) {
-    mapping::proto::Submap2D submap_2d;
-    *submap_2d.mutable_local_pose() = submap_in.submap_2d().local_pose();
-    submap_2d.set_num_range_data(submap_in.submap_2d().num_range_data());
-    submap_2d.set_finished(submap_in.submap_2d().finished());
+  CHECK(HasLegacyProbabilityGrid2d(submap_in));
+  mapping::proto::Submap2D submap_2d;
 
-    // grid() is a disguised legacy probability_grid() - migrate it.
-    // We can't directly convert it because the proto field numbers were changed
-    // in a non-compatible way :( ---> known_cells_box was 8, now 3 .... meh
-    //std::string probability_grid_binary;
-    //submap_in.submap_2d().SerializeToString(&probability_grid_binary);
-    //mapping::proto::LegacySubmap2D tmp;
-    //tmp.ParseFromString(probability_grid_binary);
-    //CHECK(tmp.has_local_pose());
-    //CHECK_NE(tmp.num_range_data(), 0);
-    //CHECK_NE(tmp.finished(), 0);
-    CHECK(submap_in.submap_2d().has_probability_grid());
-    submap_2d.mutable_grid()->mutable_limits()->CopyFrom(submap_in.submap_2d().probability_grid().limits());
-    submap_2d.mutable_grid()->mutable_cells()->CopyFrom(submap_in.submap_2d().probability_grid().cells());
-
-    for (auto& cell : *submap_2d.mutable_grid()->mutable_cells()) {
-      cell = -1 * cell;
-    }
-
-    // TODO: these make problems:
-    //LOG(INFO) << std::to_string(submap_in.submap_2d().probability_grid().known_cells_box().max_y());
-    //LOG(INFO) << std::to_string(tmp.probability_grid().known_cells_box().max_y());
-
-    submap_2d.mutable_grid()->mutable_known_cells_box()->set_max_x(
-        submap_in.submap_2d().probability_grid().known_cells_box().max_x());
-    submap_2d.mutable_grid()->mutable_known_cells_box()->set_max_y(
-        submap_in.submap_2d().probability_grid().known_cells_box().max_y());
-    submap_2d.mutable_grid()->mutable_known_cells_box()->set_min_x(
-        submap_in.submap_2d().probability_grid().known_cells_box().min_x());
-    submap_2d.mutable_grid()->mutable_known_cells_box()->set_min_y(
-        submap_in.submap_2d().probability_grid().known_cells_box().min_y());
-    
-    //submap_2d.mutable_grid()->set_max_correspondence_cost(0);
-    //submap_2d.mutable_grid()->set_min_correspondence_cost(-1);
-    submap_2d.mutable_grid()->mutable_probability_grid_2d();
-
-
-    mapping::proto::Submap submap_out;
-    *submap_out.mutable_submap_2d() = submap_2d;
-    *submap_out.mutable_submap_id() = submap_in.submap_id();
-    return submap_out;
-  } else {
-    // Already in newer format - nothing to do.
-    return mapping::proto::Submap();
+  // Convert probability grid to generalized grid.
+  CHECK(submap_in.submap_2d().has_probability_grid());
+  *submap_2d.mutable_grid()->mutable_limits() =
+      submap_in.submap_2d().probability_grid().limits();
+  *submap_2d.mutable_grid()->mutable_cells() =
+      submap_in.submap_2d().probability_grid().cells();
+  for (auto& cell : *submap_2d.mutable_grid()->mutable_cells()) {
+    cell = -1 * cell;
   }
+  // CellBox can't be trivially copied because the protobuf
+  // serialization field number doesn't match.
+  submap_2d.mutable_grid()->mutable_known_cells_box()->set_max_x(
+      submap_in.submap_2d().probability_grid().known_cells_box().max_x());
+  submap_2d.mutable_grid()->mutable_known_cells_box()->set_max_y(
+      submap_in.submap_2d().probability_grid().known_cells_box().max_y());
+  submap_2d.mutable_grid()->mutable_known_cells_box()->set_min_x(
+      submap_in.submap_2d().probability_grid().known_cells_box().min_x());
+  submap_2d.mutable_grid()->mutable_known_cells_box()->set_min_y(
+      submap_in.submap_2d().probability_grid().known_cells_box().min_y());
+
+  // Correspondence costs can be safely set to standard values.
+  // Otherwise, this would be done during next deserialization, but with a
+  // warning.
+  submap_2d.mutable_grid()->set_max_correspondence_cost(
+      mapping::kMaxCorrespondenceCost);
+  submap_2d.mutable_grid()->set_min_correspondence_cost(
+      mapping::kMinCorrespondenceCost);
+  submap_2d.mutable_grid()->mutable_probability_grid_2d();
+  *submap_2d.mutable_local_pose() = submap_in.submap_2d().local_pose();
+  submap_2d.set_num_range_data(submap_in.submap_2d().num_range_data());
+  submap_2d.set_finished(submap_in.submap_2d().finished());
+
+  mapping::proto::Submap submap_out;
+  *submap_out.mutable_submap_2d() = submap_2d;
+  *submap_out.mutable_submap_id() = submap_in.submap_id();
+  return submap_out;
 }
 
 bool DeserializeNext(cartographer::io::ProtoStreamReaderInterface* const input,
@@ -128,8 +117,64 @@ bool DeserializeNext(cartographer::io::ProtoStreamReaderInterface* const input,
     LOG_FIRST_N(INFO, 1) << "Migrating submap data.";
     auto& output_vector = (*proto_map)[SerializedData::kSubmapFieldNumber];
     output_vector.emplace_back();
+    *output_vector.back().mutable_submap() = legacy_data.submap();
+  }
+  if (legacy_data.has_node()) {
+    LOG_FIRST_N(INFO, 1) << "Migrating node data.";
+    auto& output_vector = (*proto_map)[SerializedData::kNodeFieldNumber];
+    output_vector.emplace_back();
+    *output_vector.back().mutable_node() = legacy_data.node();
+  }
+  if (legacy_data.has_trajectory_data()) {
+    LOG_FIRST_N(INFO, 1) << "Migrating trajectory data.";
+    auto& output_vector =
+        (*proto_map)[SerializedData::kTrajectoryDataFieldNumber];
+    output_vector.emplace_back();
+    *output_vector.back().mutable_trajectory_data() =
+        legacy_data.trajectory_data();
+  }
+  if (legacy_data.has_imu_data()) {
+    LOG_FIRST_N(INFO, 1) << "Migrating IMU data.";
+    auto& output_vector = (*proto_map)[SerializedData::kImuDataFieldNumber];
+    output_vector.emplace_back();
+    *output_vector.back().mutable_imu_data() = legacy_data.imu_data();
+  }
+  if (legacy_data.has_odometry_data()) {
+    LOG_FIRST_N(INFO, 1) << "Migrating odometry data.";
+    auto& output_vector = (*proto_map)[SerializedData::kOdometryData];
+    output_vector.emplace_back();
+    *output_vector.back().mutable_odometry_data() = legacy_data.odometry_data();
+  }
+  if (legacy_data.has_fixed_frame_pose_data()) {
+    LOG_FIRST_N(INFO, 1) << "Migrating fixed frame pose data.";
+    auto& output_vector =
+        (*proto_map)[SerializedData::kFixedFramePoseDataFieldNumber];
+    output_vector.emplace_back();
+    *output_vector.back().mutable_fixed_frame_pose_data() =
+        legacy_data.fixed_frame_pose_data();
+  }
+  if (legacy_data.has_landmark_data()) {
+    LOG_FIRST_N(INFO, 1) << "Migrating landmark data.";
+    auto& output_vector =
+        (*proto_map)[SerializedData::kLandmarkDataFieldNumber];
+    output_vector.emplace_back();
+    *output_vector.back().mutable_landmark_data() = legacy_data.landmark_data();
+  }
+  return true;
+}
+
+bool DeserializeNextWithLegacySubmap(
+    cartographer::io::ProtoStreamReaderInterface* const input,
+    ProtoMap* proto_map) {
+  mapping::proto::LegacySerializedDataOldSubmap legacy_data;
+  if (!input->ReadProto(&legacy_data)) return false;
+
+  if (legacy_data.has_submap()) {
+    LOG_FIRST_N(INFO, 1) << "Migrating submap data.";
+    auto& output_vector = (*proto_map)[SerializedData::kSubmapFieldNumber];
+    output_vector.emplace_back();
     *output_vector.back().mutable_submap() =
-        MaybeMigrateLegacySubmap2d(legacy_data.submap());
+        MigrateLegacySubmap2d(legacy_data.submap());
   }
   if (legacy_data.has_node()) {
     LOG_FIRST_N(INFO, 1) << "Migrating node data.";
@@ -176,7 +221,8 @@ bool DeserializeNext(cartographer::io::ProtoStreamReaderInterface* const input,
 }
 
 ProtoMap ParseLegacyData(
-    cartographer::io::ProtoStreamReaderInterface* const input) {
+    cartographer::io::ProtoStreamReaderInterface* const input,
+    bool uses_old_submap) {
   ProtoMap proto_map;
   CHECK(ReadPoseGraph(input, &proto_map))
       << "Input stream seems to differ from original stream format. Could "
@@ -186,8 +232,13 @@ ProtoMap ParseLegacyData(
       << "Input stream seems to differ from original stream format. Could "
          "not "
          "read AllTrajectoryBuilderOptions as second message.";
-  do {
-  } while (DeserializeNext(input, &proto_map));
+  if (uses_old_submap) {
+    do {
+    } while (DeserializeNextWithLegacySubmap(input, &proto_map));
+  } else {
+    do {
+    } while (DeserializeNext(input, &proto_map));
+  }
   return proto_map;
 }
 
@@ -225,8 +276,9 @@ void SerializeToVersion1Format(
 
 void MigrateStreamFormatToVersion1(
     cartographer::io::ProtoStreamReaderInterface* const input,
-    cartographer::io::ProtoStreamWriterInterface* const output) {
-  SerializeToVersion1Format(ParseLegacyData(input), output);
+    cartographer::io::ProtoStreamWriterInterface* const output,
+    bool uses_old_submap) {
+    SerializeToVersion1Format(ParseLegacyData(input, uses_old_submap), output);
 }
 
 }  // namespace io
